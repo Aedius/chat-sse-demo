35419906d1a00f16244d6f5defd0396eb3b03bbf
aedius <aedius.filmania@gmail.com>
2019-10-03 13:57:30 +0200

    remove Pool

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,25 @@ use std::net::TcpListener;
 use std::fs;
 use std::thread;
 use std::time::Duration;
-use chat_sse_demo::ThreadPool;
 use chrono;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let pool = ThreadPool::new(100);
-
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        thread::spawn(
+            move || {
+                match stream{
+                    Ok(str) => {
 
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+                        handle_connection(str);
+                    },
+                    Err(err) => {
+                        println!("{}", err)
+                    }
+                }
+            }
+        );
+
     }
 
     println!("Shutting down.");
