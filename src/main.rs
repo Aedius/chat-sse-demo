b77c85dc2dcfaabb3293dbfc50e496c38213b313
Aedius <aedius.filmania@gmail.com>
2019-10-12 23:58:36 +0200

    adding multiplexer

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,86 @@ use std::fs;
 use std::thread;
 use std::time::Duration;
 use chrono;
-use std::sync::mpsc::{channel, Receiver};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver, Sender};
 
+#[derive(Clone)]
 pub struct Message {
     name: String,
     content: String,
+}
+
+pub enum MMess {
+    Message(Message),
+    Sender(Sender<Message>),
+}
+
+pub struct Multiplex {
+    receiver: Receiver<MMess>,
+    sender_list: Vec<Sender<Message>>,
+}
+
+impl Multiplex {
+    pub fn new(receiver: Receiver<MMess>) -> Multiplex {
+        Multiplex {
+            receiver,
+            sender_list: Vec::new(),
+        }
+    }
+
+    pub fn add_sender(mut self, sender: Sender<Message>) {
+        self.sender_list.push(sender)
+    }
+
+    pub fn listen(mut self) {
+        loop {
+            match self.receiver.recv() {
+                Ok(multi_message) => {
+                    match multi_message {
+                        MMess::Message(mess) => {
+                            let mut dead_channel = Vec::new();
+                            println!("new message ! Sending to {} chanel", &self.sender_list.len());
+                            for (pos,sender) in self.sender_list.iter().enumerate() {
+                                match sender.send(mess.clone()) {
+                                    Ok(()) => {}
+                                    Err(e) => {
+                                        dead_channel.push(pos);
+                                        println!("multiplex cannot send {}, removing", e);
+                                    }
+                                }
+                            }
+                            for pos in dead_channel{
+                                self.sender_list.remove(pos);
+                            }
+                        }
+                        MMess::Sender(sender) => {
+                            println!("new Listener !");
+                            self.sender_list.push(sender)
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("multiplex error receiving : {}", e);
+                }
+            }
+        }
+    }
 }
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    println!("listen on 8080");
+
     let (sender, receiver) = channel();
 
+    let multiplex = Multiplex::new(receiver);
+
+    thread::spawn(
+        || multiplex.listen()
+    );
+
+    let sender_clock = sender.clone();
     thread::spawn(
         move || {
             loop {
@@ -29,10 +95,10 @@ fn main() {
                     content: format!("{}{}", "This is a message at time ", date),
                 };
 
-                match sender.send(mess){
-                    Ok(()) => {},
+                match sender_clock.send(MMess::Message(mess)) {
+                    Ok(()) => {}
                     Err(e) => {
-                        println!("cannot send {}", e);
+                        println!("clock cannot send {}", e);
                     }
                 }
                 thread::sleep(Duration::from_secs(1));
@@ -40,16 +106,23 @@ fn main() {
         }
     );
 
-    let receiver= Arc::new(Mutex::new(receiver)) ;
-
-
     for stream in listener.incoming() {
-        let receiver_move = receiver.clone();
+
+        let sender = sender.clone();
+        let (sender_stream, receiver_stream) = channel();
+
+        match sender.send(MMess::Sender(sender_stream)){
+            Ok(()) => {},
+            Err(err) => {
+                println!("{}", err)
+            }
+        }
+
         match stream {
             Ok(str) => {
                 thread::spawn(
                     || {
-                        handle_connection(str, receiver_move);
+                        handle_connection(str, receiver_stream);
                     }
                 );
             }
@@ -61,8 +134,7 @@ fn main() {
 }
 
 
-
-fn handle_connection(mut stream: TcpStream, receiver: Arc<Mutex<Receiver<Message>>>) {
+fn handle_connection(mut stream: TcpStream, receiver: Receiver<Message>) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
@@ -94,7 +166,7 @@ fn handle_connection(mut stream: TcpStream, receiver: Arc<Mutex<Receiver<Message
     }
 }
 
-pub fn process_sse(stream: &mut TcpStream, mutex: Arc<Mutex<Receiver<Message>>>) -> Result<(), std::io::Error> {
+pub fn process_sse(stream: &mut TcpStream, receiver: Receiver<Message>) -> Result<(), std::io::Error> {
     let headers = [
         "HTTP/1.1 200 OK",
         "Content-Type: text/event-stream",
@@ -107,30 +179,22 @@ pub fn process_sse(stream: &mut TcpStream, mutex: Arc<Mutex<Receiver<Message>>>)
     stream.write(&response)?;
 
     loop {
-
-        match mutex.try_lock(){
-            Ok(receiver) => {
-                match receiver.recv() {
-                    Ok(message) => {
-                        let response = format!("{}{}{}", "event: ", message.name, "\r\n");
-                        stream.write(response.as_bytes())?;
-                        let response = format!("{}{}{}", "data:", message.content, "\r\n\r\n");
-                        stream.write(response.as_bytes())?;
-                        match stream.flush() {
-                            Ok(()) => {}
-                            Err(e) => {
-                                return Err(e);
-                            }
-                        }
-                    }
+        match receiver.recv() {
+            Ok(message) => {
+                let response = format!("{}{}{}", "event: ", message.name, "\r\n");
+                stream.write(response.as_bytes())?;
+                let response = format!("{}{}{}", "data:", message.content, "\r\n\r\n");
+                stream.write(response.as_bytes())?;
+                match stream.flush() {
+                    Ok(()) => {}
                     Err(e) => {
-                        println!("error receiving : {}", e);
-                        return Ok(());
+                        return Err(e);
                     }
                 }
-            },
+            }
             Err(e) => {
                 println!("error receiving : {}", e);
+                return Ok(());
             }
         }
     }
